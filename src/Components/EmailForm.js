9f65f5dea2f7bfd41ee9b1c44cfd7aebfbc5d97d
Rudi <rudi@kanayahijab.com>
2021-01-26 20:31:54 +0700

    Bikin form kirim email dg style pure css

--- src/Components/EmailForm.js
+++
@@ -1,9 +0,0 @@
-export class EmailForm extends HTMLElement {
-    constructor() {
-        super();
-    }
-
-    connectedCallback() {
-        this.innerHTML = '<h1>Form to send Email</h1>';
-    }
-}
--- src/client.js
+++ src/client.js
@@ -1,3 +1,17 @@
-import { EmailForm } from "./Components/EmailForm";
+(function () {
+    let sendEmailForm = document.getElementById('send-email-form');
+    sendEmailForm.onsubmit = function () {
+        let emailData = new FormData(sendEmailForm);
+        fetch(`http://${window.location.hostname}/api/emails`, {
+            method: 'POST',
+            body: emailData
+        }).then(function () {
+            window.alert('Email sent successfully');
+            sendEmailForm.reset();
+        }).catch(function (reason) {
+            window.alert(reason);
+        });
 
-customElements.define('email-form', EmailForm);
+        return false;
+    };
+})();
