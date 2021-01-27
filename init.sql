36cf2bceb2e080e3ec6d736572476485947eb468
Rudi <rudi@kanayahijab.com>
2021-01-27 19:14:32 +0700

    Bikin tabel users & user dummy admin

--- init.sql
+++ init.sql
@@ -1,4 +1,15 @@
--- Emails table
+-- Users table
+CREATE TABLE users (
+    id serial PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(60) NOT NULL
+);
+
+-- Dummy admin user, password: pass123
+INSERT INTO users (username, password)
+VALUES ('admin', '$2y$10$BmkKxsQaNalxw4ep4BWEPeoK1MKyk1kCMTYulzTUj8Y4WzWVsR6zG');
+
+-- Emails table to store sent/unsent emails
 CREATE TABLE emails (
     id serial PRIMARY KEY,
     sender VARCHAR(320) NOT NULL,
